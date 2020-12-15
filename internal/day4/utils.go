28fb5a4515d23eb8f18fc63d35f4ba998880a98e
Juan A. Moyano <wincus@gmail.com>
2020-12-15 19:53:22 -0300

    add day3 solution

---
+++ internal/day4/utils.go
@@ -0,0 +1,10 @@
+package day4
+
+import (
+	"github.com/wincus/adventofcode/internal/common"
+)
+
+// Solve returns the solution for day3 problem
+func Solve(s []string, p common.Part) int {
+	return 0
+}
